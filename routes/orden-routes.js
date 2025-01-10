d23504549900fc5f70ebad6f5256bb840ce210ed
Juliangmtz <159282087+Juliangmtz@users.noreply.github.com>
2025-01-10 10:59:56 -0600

    Delete routes directory

--- routes/orden-routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ordenesControllerFinales = require('../contollers/orden-controller');
-
-router.post('/', ordenesControllerFinales.cerrarOrden); // Cerrar una orden
-
-module.exports = router;
